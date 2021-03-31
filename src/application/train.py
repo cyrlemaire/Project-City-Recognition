114a5cbf8040990228e0e2f1baa6ef40ee95bfe3
cyrlemaire <72152608+cyrlemaire@users.noreply.github.com>
2021-03-31 17:11:07 +0200

    :sparkles: Add learning rate decay schedule

--- src/application/train.py
+++ src/application/train.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 import warnings
+
 warnings.filterwarnings("ignore")
 
 from src.domain.model import make_image_scaler, make_specific_learner, base_model
 from src.infrastructure.data_generator import train_generator, valid_generator
 from src.config.config import *
-
 
 if __name__ == '__main__':
 
@@ -26,10 +26,18 @@ if __name__ == '__main__':
     print('TRAIN MODEL')
     print('===========')
 
-
+    # set up a schedule for learning rate decay
+    lr_schedule = tf.keras.optimizers.schedules.PolynomialDecay(
+        initial_learning_rate=INITIAL_LEARNING_RATE,
+        end_learning_rate=FINAL_LEARNING_RATE,
+        decay_steps=DECAY_STEPS,
+        power=1.0,
+        cycle=False,
+        name=None
+    )
 
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),
         loss='categorical_crossentropy',
         metrics='accuracy',
     )
