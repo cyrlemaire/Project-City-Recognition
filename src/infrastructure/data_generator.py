56157d18e55be0d39e42c254506a6ea9b09d29d7
Olivier Collier <oliviercollier8@gmail.com>
2021-04-03 12:28:55 +0200

    :recycle: Improving code
    Rewriting train.py, predict.py, model.py, data_generator.py and config.py

--- src/infrastructure/data_generator.py
+++ src/infrastructure/data_generator.py
@@ -1,64 +1,46 @@
+
 import tensorflow as tf
 import warnings
-
 warnings.filterwarnings("ignore")
 
-from src.config.config import *
-
-# loading arguments for generators
-
-dataflow_kwargs = dict(target_size=IMAGE_SIZE,
-                       batch_size=BATCH_SIZE,
-                       interpolation=INTERPOLATION
-                       )
-
-# Instanciate image data generator for validation
-valid_datagen = tf.keras.preprocessing.image.ImageDataGenerator(validation_split=VALIDATION_SPLIT)
-
-# Create validation dataset
-valid_generator = valid_datagen.flow_from_directory(TRAIN_DATA_DIR,
-                                                    subset="validation",
-                                                    shuffle=False,
-                                                    **dataflow_kwargs
-                                                    )
-
-if D0_DATA_AUGMENTATION:
-
-    # Instanciate image data generator for train batchs with augmantation
-    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=12,
-                                                                    horizontal_flip=True,
-                                                                    width_shift_range=0.1,
-                                                                    height_shift_range=0.1,
-                                                                    shear_range=0.2,
-                                                                    zoom_range=0.3,
-                                                                    channel_shift_range=20,
-                                                                    validation_split=VALIDATION_SPLIT
-                                                                    )
-else:
-
-    # Instanciate image data generator for train batchs without augmentation
-    train_datagen = valid_datagen
-
-# generator for train dataset
-train_generator = train_datagen.flow_from_directory(TRAIN_DATA_DIR,
-                                                    subset="training",
-                                                    shuffle=True,
-                                                    **dataflow_kwargs
-                                                    )
+import src.config.config as config
 
 
-# Load data for test
+def train_validation_data_generator():
+    """Loads generators for train and validation data."""
+    dataflow_kwargs = dict(target_size=config.IMAGE_SIZE,
+                           batch_size=config.BATCH_SIZE,
+                           interpolation=config.INTERPOLATION)
+    train_data_generator = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=12,
+                                                                           horizontal_flip=True,
+                                                                           width_shift_range=0.1,
+                                                                           height_shift_range=0.1,
+                                                                           shear_range=0.2,
+                                                                           zoom_range=0.3,
+                                                                           channel_shift_range=20,
+                                                                           validation_split=config.VALIDATION_SPLIT)
+    train_generator = train_data_generator.flow_from_directory(config.TRAIN_DATA_DIR,
+                                                               subset="training",
+                                                               shuffle=True,
+                                                               **dataflow_kwargs)
+    validation_data_generator = tf.keras.preprocessing.image.ImageDataGenerator(validation_split=config.VALIDATION_SPLIT)
+    validation_generator = validation_data_generator.flow_from_directory(config.TRAIN_DATA_DIR,
+                                                                         subset="validation",
+                                                                         shuffle=False,
+                                                                         **dataflow_kwargs)
+    return train_generator, validation_generator
+
 
 def load_test_data():
-
+    """Loads test data."""
     test_data = tf.keras.preprocessing.image_dataset_from_directory(
-        TEST_DATA_DIR,
+        config.TEST_DATA_DIR,
         labels="inferred",
         label_mode="categorical",
-        class_names=CLASSES,
-        batch_size=BATCH_SIZE,
+        class_names=config.CLASSES,
+        batch_size=config.BATCH_SIZE,
         shuffle=False,
-        image_size=IMAGE_SIZE,
+        image_size=config.IMAGE_SIZE,
         validation_split=None
     )
 
